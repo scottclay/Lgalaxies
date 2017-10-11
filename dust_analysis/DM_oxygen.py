2ac1aa1ac222b508b9256812421c03b924a25a66
scottclay <scottclay8@gmail.com>
2017-10-11 19:04:08 +0100

    Update most plots to use new bin_data function

--- dust_analysis/DM_oxygen.py
+++ dust_analysis/DM_oxygen.py
@@ -18,79 +18,51 @@ from read_pickled_data import make_selection
 from plot_params import plot_params
 from fit_scatter import fit_median
 
+from bin_data import bin_data
+
 fig, axs = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True, figsize=(9,9))
 ax = axs.reshape(-1)
+normalize=0
 fig.subplots_adjust(hspace=0)
 fig.subplots_adjust(wspace=0)
 
+
 for loop in range(0,9):
-
-	try: 
-		bin_centres,median,per_50,per_16,per_84,per_25,per_75 = np.loadtxt('./binned_data/DM_oxygen_'+str(loop)+'.txt',unpack=True,comments='#')
-		try: 
-			ax[loop] = cloudpickle.load(open('./pkl_hists/CG_oxygen_z'+str(loop)+'.pkl','rb'))
-		except IOError:
-		    	pass
-	except IOError:
-		print("Missing data - will create")
-		#df = fetch_lgalaxies(redshift=loop, data_path = '../prepare_output/',simulation='MR')
-		df = fetch_lgalaxies(redshift=loop,simulation='MR')
-		df = make_selection(df,redshift=loop)
-		DM = np.log10(df['Dust_Mass'])
-		OX_M = df['O']
-		Hyd = df['H']
-        
-		OX_Z = np.log10   (( (OX_M) /Hyd) * (1.0/16.0)) + 12.0
-    	
-		DM = DM.as_matrix()
-	
-		from fit_scatter import fit_median
-		median, bin_centres, per_50,per_16,per_84,per_25,per_75 = fit_median(OX_Z,DM,30)
-		np.savetxt('./binned_data/DM_oxygen_'+str(loop)+'.txt',np.c_[bin_centres,median,per_50,per_16,per_84,per_25,per_75])
-		try: 
-			ax[loop] = cloudpickle.load(open('./pkl_hists/DM_oxygen_z'+str(loop)+'.pkl','rb'))
-		except:
-			print("generating hists")
-			if loop == 0: 
-				hb = plt.hexbin(OX_Z,DM,gridsize=150,bins='log',mincnt=5,cmap='gist_gray')
-				min = hb.norm.vmin
-				max = hb.norm.vmax
-				normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
-				print(min,max)
-			else:
-				plt.hexbin(OX_Z,DM,gridsize=150,bins='log',mincnt=5,cmap='gist_gray',norm=normalize)
-			import pickle   
-	
-			fout = open('./pkl_hists/DM_oxygen_z'+str(loop)+'.pkl','wb')
-			cloudpickle.dump(ax[loop],fout)
-
-	plt.subplot(3,3,loop+1)
-	plt.xlim([6.,9.98])
-	plt.ylim([0,9.98])
-
-	plot_params(loop,'O','DM')
-
-	'''
-	if loop == 0: 
-		hb = plt.hexbin(OX_Z,DTG,gridsize=150,bins='log',mincnt=5,cmap='gist_heat')
-
-		min = hb.norm.vmin
-		max = hb.norm.vmax
-		normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
-		print(min,max)
-	else:
-		plt.hexbin(OX_Z,DTG,gridsize=150,bins='log',mincnt=5,cmap='gist_heat',norm=normalize)
-	'''
-
-	#plot_observations(loop,"DTG_Oxy")
-
-	plt.plot(bin_centres,per_50,c='k',zorder=10,linewidth=2,label='L-Galaxies')
-	plt.plot(bin_centres,per_16,'k--',zorder=10,linewidth=2)
-	plt.plot(bin_centres,per_84,'k--',zorder=10,linewidth=2)
+    try: 
+        bin_centres,median,per_50,per_16,per_84,per_25,per_75 = np.loadtxt('./binned_data/DM_OX_z'+str(loop)+'.txt',unpack=True,comments='#')
+        ax[loop] = cloudpickle.load(open('./pkl_hists/DM_OX_z'+str(loop)+'.pkl','rb'))
+    except IOError:
+        bin_centres,median,per_50,per_16,per_84,per_25,per_75,normalize = bin_data('DM','OX_Z',ax,normalize,loop,'DM_OX',nbins=30)
 
 
-	if loop==8:
-		plt.legend(loc='lower right',fontsize = 8)
+
+    plt.subplot(3,3,loop+1)
+    plt.xlim([6.,9.98])
+    plt.ylim([0,9.98])
+
+    plot_params(loop,'O','DM')
+
+    '''
+    if loop == 0: 
+        hb = plt.hexbin(OX_Z,DTG,gridsize=150,bins='log',mincnt=5,cmap='gist_heat')
+
+        min = hb.norm.vmin
+        max = hb.norm.vmax
+        normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
+        print(min,max)
+    else:
+        plt.hexbin(OX_Z,DTG,gridsize=150,bins='log',mincnt=5,cmap='gist_heat',norm=normalize)
+    '''
+
+    #plot_observations(loop,"DTG_Oxy")
+
+    plt.plot(bin_centres,per_50,c='k',zorder=10,linewidth=2,label='L-Galaxies')
+    plt.plot(bin_centres,per_16,'k--',zorder=10,linewidth=2)
+    plt.plot(bin_centres,per_84,'k--',zorder=10,linewidth=2)
+
+
+    if loop==8:
+        plt.legend(loc='lower right',fontsize = 8)
 
 axes = fig.get_axes()
 for ax in axes:
--- dust_analysis/DTM_stellar.py
+++ dust_analysis/DTM_stellar.py
@@ -18,81 +18,51 @@ from read_pickled_data import make_selection
 from plot_params import plot_params
 from fit_scatter import fit_median
 
+from bin_data import bin_data
+
 fig, axs = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True, figsize=(9,9))
 ax = axs.reshape(-1)
+normalize=0
 fig.subplots_adjust(hspace=0)
 fig.subplots_adjust(wspace=0)
 
+
 for loop in range(0,9):
-	try: 
-		bin_centres,median,per_50,per_16,per_84,per_25,per_75 = np.loadtxt('./binned_data/DTM_stellar_'+str(loop)+'.txt',unpack=True,comments='#')
-		try: 
-			ax[loop] = cloudpickle.load(open('./pkl_hists/DTM_stellar_z'+str(loop)+'.pkl','rb'))
-		except IOError:
-		    	pass
-	except IOError:
-		print("Missing data - will create")
-		#df = fetch_lgalaxies(redshift=loop, data_path = '../prepare_output/',simulation='MR')
-		df = fetch_lgalaxies(redshift=loop,simulation='MR')
-		df = make_selection(df,redshift=loop)
-		DM = np.log10(df['Dust_Mass'])
-		SM = np.log10(df['StellarMass'])
-		#MM = np.log10(df['Metal_Mass']+df['Dust_Mass'])
-		MM = np.log10(df['Metal_Mass'])
-
-		DTM = DM - MM
-	
-		DTM = DTM.as_matrix()
-	
-		from fit_scatter import fit_median
-		median, bin_centres, per_50,per_16,per_84,per_25,per_75 = fit_median(SM,DTM,30)
-		np.savetxt('./binned_data/DTM_stellar_'+str(loop)+'.txt',np.c_[bin_centres,median,per_50,per_16,per_84,per_25,per_75])
-		try: 
-			ax[loop] = cloudpickle.load(open('./pkl_hists/DTM_stellar_z'+str(loop)+'.pkl','rb'))
-		except:
-			print("generating hists")
-			if loop == 0: 
-				hb = plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_gray')
-				min = hb.norm.vmin
-				max = hb.norm.vmax
-				normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
-				print(min,max)
-			else:
-				plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_gray',norm=normalize)
-			import pickle   
-	
-			fout = open('./pkl_hists/DTM_stellar_z'+str(loop)+'.pkl','wb')
-			cloudpickle.dump(ax[loop],fout)
+    try: 
+        bin_centres,median,per_50,per_16,per_84,per_25,per_75 = np.loadtxt('./binned_data/SM_DTM_z'+str(loop)+'.txt',unpack=True,comments='#')
+        ax[loop] = cloudpickle.load(open('./pkl_hists/SM_DTM_z'+str(loop)+'.pkl','rb'))
+    except IOError:
+        bin_centres,median,per_50,per_16,per_84,per_25,per_75,normalize = bin_data('SM','DTM',ax,normalize,loop,'SM_DTM',nbins=30)
 
 
-	plt.subplot(3,3,loop+1)
-	plt.xlim([8.,11.98])
-	plt.ylim([-3.98,2])
-	
-	plot_params(loop,'SM','DTM')
-	
-	'''
-	if loop == 0: 
-		hb = plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_heat')
-	
-		min = hb.norm.vmin
-		max = hb.norm.vmax
-		normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
-		print(min,max)
-	else:
-		plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_heat',norm=normalize)
+    plt.subplot(3,3,loop+1)
+    plt.xlim([8.,11.98])
+    plt.ylim([-3.98,2])
+    
+    plot_params(loop,'SM','DTM')
+    
     '''
-	plot_observations(loop,"DTM_SM")
-	#plt.errorbar(x_bins,y_median,yerr=(y_mederr),color='k',label='L-Galaxies Median',linewidth=2)
-	
-	plt.plot(bin_centres,per_50,c='k',zorder=10,linewidth=2,label='L-Galaxies')
-	plt.plot(bin_centres,per_16,'k--',zorder=10,linewidth=2)
-	plt.plot(bin_centres,per_84,'k--',zorder=10,linewidth=2)
-	
-	
-	#plt.text(8.2,1.0,"z = "+str(loop), fontsize = 16)
-	if loop==8:
-		plt.legend(loc='lower right',fontsize = 8)
+    if loop == 0: 
+        hb = plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_heat')
+    
+        min = hb.norm.vmin
+        max = hb.norm.vmax
+        normalize = matplotlib.colors.Normalize(vmin=min, vmax=max)
+        print(min,max)
+    else:
+        plt.hexbin(SM,DTM,gridsize=150,bins='log',mincnt=5,cmap='gist_heat',norm=normalize)
+    '''
+    plot_observations(loop,"DTM_SM")
+    #plt.errorbar(x_bins,y_median,yerr=(y_mederr),color='k',label='L-Galaxies Median',linewidth=2)
+    
+    plt.plot(bin_centres,per_50,c='k',zorder=10,linewidth=2,label='L-Galaxies')
+    plt.plot(bin_centres,per_16,'k--',zorder=10,linewidth=2)
+    plt.plot(bin_centres,per_84,'k--',zorder=10,linewidth=2)
+    
+    
+    #plt.text(8.2,1.0,"z = "+str(loop), fontsize = 16)
+    if loop==8:
+        plt.legend(loc='lower right',fontsize = 8)
 
 axes = fig.get_axes()
 for ax in axes:
